fa3cb5bebdef1cf0d1ba9763276ab5ea189743ba
Dumindu Harshana <ruvinduharshana536@gmail.com>
2024-06-03 09:10:47 +0530

    Add files via upload

---
+++ src/main/java/com/capstone/ttp/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.capstone.ttp.repositories;
+
+
+import com.capstone.ttp.entitiy.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
---
+++ src/main/java/com/capstone/ttp/services/AuthenticationService.java
@@ -0,0 +1,50 @@
+package com.capstone.ttp.services;
+
+import com.capstone.ttp.dtos.LoginUserDto;
+import com.capstone.ttp.dtos.RegisterUserDto;
+import com.capstone.ttp.entitiy.User;
+import com.capstone.ttp.repositories.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationService(
+            UserRepository userRepository,
+            AuthenticationManager authenticationManager,
+            PasswordEncoder passwordEncoder
+    ) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User signup(RegisterUserDto input) {
+        User user = new User()
+                .setFullName(input.getFullName())
+                .setEmail(input.getEmail())
+                .setPassword(passwordEncoder.encode(input.getPassword()));
+
+        return userRepository.save(user);
+    }
+
+    public User authenticate(LoginUserDto input) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        input.getEmail(),
+                        input.getPassword()
+                )
+        );
+
+        return userRepository.findByEmail(input.getEmail())
+                .orElseThrow();
+    }
+}
