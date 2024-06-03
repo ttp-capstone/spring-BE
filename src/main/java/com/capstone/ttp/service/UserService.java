8f39182df08e9e72d7a6e395aa5c2c1815a431b8
Dumindu Harshana <ruvinduharshana536@gmail.com>
2024-06-02 23:37:25 -0400

    Delete src directory

--- src/main/java/com/capstone/ttp/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.capstone.ttp.service;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService extends UserDetailsService {
-
-    public abstract boolean loginUser(String username, String password);
-}
