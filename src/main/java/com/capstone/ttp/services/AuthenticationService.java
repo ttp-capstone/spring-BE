94e797d2026493dbb403025a0e97230982b1090c
Dumindu Harshana <ruvinduharshana536@gmail.com>
2024-06-05 22:02:11 -0400

    Revert "added role based access"

--- src/main/java/com/capstone/ttp/services/AuthenticationService.java
+++ src/main/java/com/capstone/ttp/services/AuthenticationService.java
@@ -2,25 +2,19 @@ package com.capstone.ttp.services;
 
 import com.capstone.ttp.dtos.LoginUserDto;
 import com.capstone.ttp.dtos.RegisterUserDto;
-import com.capstone.ttp.entitiy.Role;
-import com.capstone.ttp.entitiy.RoleEnum;
 import com.capstone.ttp.entitiy.User;
 import com.capstone.ttp.repositories.UserRepository;
-import com.capstone.ttp.repositories.RoleRepository;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-
-import java.util.Optional;
 
 @Service
 public class AuthenticationService {
     private final UserRepository userRepository;
 
     private final PasswordEncoder passwordEncoder;
-    RoleRepository roleRepository;
+
     private final AuthenticationManager authenticationManager;
 
     public AuthenticationService(
@@ -34,17 +28,10 @@ public class AuthenticationService {
     }
 
     public User signup(RegisterUserDto input) {
-        Optional<Role> optionalRole = roleRepository.findByName(RoleEnum.USER);
-
-        if (optionalRole.isEmpty()) {
-            return null;
-        }
-
-        var user = new User()
+        User user = new User()
                 .setFullName(input.getFullName())
                 .setEmail(input.getEmail())
-                .setPassword(passwordEncoder.encode(input.getPassword()))
-                .setRole(optionalRole.get());
+                .setPassword(passwordEncoder.encode(input.getPassword()));
 
         return userRepository.save(user);
     }
